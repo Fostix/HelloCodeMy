c1212a3b42afe7f8e36bac41e985e8c2ce90f390
Fostix <accretiondiskthat@gmail.com>
2021-11-21 21:31:58 +0500

    Did task 0, 1, 2, 3, 5.

---
+++ Task003_ShowFromSecondNumbersWhichMoreAndLess/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter first number: ");
+double numA = double.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+double numB = double.Parse(Console.ReadLine());
+if(numA > numB)
+{
+    Console.Write("first number: ");
+    Console.Write(numA);
+    Console.Write(" more than second number: ");
+    Console.Write(numB);
+    Console.Write(".");
+}
+else
+{
+    Console.Write("first number: ");
+    Console.Write(numA);
+    Console.Write(" Less than second number: ");
+    Console.Write(numB);
+    Console.Write(".");
+}
