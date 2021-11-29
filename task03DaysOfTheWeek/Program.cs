cb5ee27f77997fc7c100b529d8e02e39c2b9613e
Fostix <accretiondiskthat@gmail.com>
2021-11-29 13:28:55 +0500

    trayTask3_dayoftheweek

---
+++ task03DaysOfTheWeek/Program.cs
@@ -0,0 +1,12 @@
+﻿//По заданному номеру дня недели вывести его название
+string[] numdDay = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+Console.Write("Enter number: and i will write day of the week: ");
+int numDay = int.Parse(Console.Read());
+
+numdDay = numDay + 1;
+
+
+
+
+
+/* Console.Write("and i will write day of the week"); */
