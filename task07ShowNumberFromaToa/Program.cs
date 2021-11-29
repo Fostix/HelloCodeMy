a8cb1fe79e26ac1b64f034caa1f7c7ae32c6ff8c
Fostix <accretiondiskthat@gmail.com>
2021-11-29 21:36:39 +0500

    task08evennumbersfrom1towhatyouenter

---
+++ task07ShowNumberFromaToa/Program.cs
@@ -0,0 +1,11 @@
+﻿//Показать числа от -N до N
+Console.Write("Enter from number: ");
+int from = int.Parse(Console.ReadLine());
+Console.Write("Enter stop number: ");
+int to = int.Parse(Console.ReadLine());
+to = to + 1;
+
+for (int shownumb = from; shownumb < to; shownumb++)
+{
+    Console.WriteLine(shownumb);
+}
