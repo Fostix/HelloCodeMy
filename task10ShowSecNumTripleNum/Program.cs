c27f843fe92eda0dd04a25e695061fb02d41a14a
Fostix <accretiondiskthat@gmail.com>
2021-11-29 21:52:44 +0500

    task10SecNumFromTriple

---
+++ task10ShowSecNumTripleNum/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Enter triple number: ");
+int trip = int.Parse(Console.ReadLine());
+
+int sec = trip % 100; // остаток не просто деление
+sec = sec / 10; // обычное деление
+
+Console.Write("second number from triple number: ");
+Console.WriteLine(sec);
