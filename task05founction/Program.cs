2d7108a49a1e92c33a04457c2543c85e4a532581
Fostix <accretiondiskthat@gmail.com>
2021-11-29 19:31:10 +0500

    Even Odd Number

---
+++ task05founction/Program.cs
@@ -0,0 +1,11 @@
+﻿//Написать программу вычисления значения функции y = f(a)
+Console.Write("Enter first number: ");
+int f = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int a = int.Parse(Console.ReadLine());
+
+
+int y = Convert.ToInt32(Math.Pow(f, a));
+y = y + 1;
+Console.Write("hello: ");
+Console.WriteLine(y);
---
+++ task06eveoddnumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 1) // div
+{
+    Console.WriteLine("odd");
+}
+else
+{
+    Console.WriteLine("even");
+}
