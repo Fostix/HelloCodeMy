3a3143fd87cd3189b293967159f2544741fbd6a5
Fostix <accretiondiskthat@gmail.com>
2021-11-29 18:42:47 +0500

    needMaketask03

--- task03DaysOfTheWeek/Program.cs
+++ task03DaysOfTheWeek/Program.cs
@@ -1,9 +1,39 @@
 ﻿//По заданному номеру дня недели вывести его название
-string[] numdDay = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+string[] numdDay = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
 Console.Write("Enter number: and i will write day of the week: ");
-int numDay = int.Parse(Console.Read());
+int numDay = int.Parse(Console.ReadLine());
+numDay = numDay - 1;
 
-numdDay = numDay + 1;
+if (numDay < 7)
+{
+    Console.WriteLine(numdDay[numDay]);
+    
+}
+else
+{
+    numDay = numDay - 7;
+}
+
+
+/* 
+for (int i = 99; i > numDay; i=-7)
+    Console.WriteLine(i);
+
+
+ */
+
+
+
+/* if (numDay < 7)
+{
+    Console.WriteLine(numdDay[numDay]);
+}
+else
+{
+    numDay = numDay - 7;
+    return numdDay;
+}
+ */
 
 
 
