5104f886049d0e0cb0b9b70d6708313982149b93
Fostix <accretiondiskthat@gmail.com>
2021-11-29 19:19:31 +0500

    Task05Founction

---
+++ task02ShowLessAndMoreNum/Program.cs
@@ -0,0 +1,10 @@
+﻿//Даны два числа. Показать большее и меньшее число
+Console.Write("Enter first number: ");
+int firstN = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondN = int.Parse(Console.ReadLine());
+
+if (firstN < secondN) Console.WriteLine($"first number: {firstN} less than second number: {secondN}.");
+else
+    Console.WriteLine($"first number: {firstN} more than second number: {secondN}.");
+if (firstN == secondN) Console.Write($"numbers equals: {firstN}, {secondN}.");
---
+++ task04FindMaxOfTheTree/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти максимальное из трех чисел
+Console.Write("enter first number: ");
+int first = int.Parse(Console.ReadLine());
+Console.Write("enter second number: ");
+int second = int.Parse(Console.ReadLine());
+Console.Write("enter three number: ");
+int three = int.Parse(Console.ReadLine());
+
+int max = first;
+
+if (second > max) max = second;
+if (three > max) max = three;
+
+Console.Write("this number max of the three numbers: ");
+Console.WriteLine(max);
