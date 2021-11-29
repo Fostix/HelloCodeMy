6c9c96e2eb2a86222c29959962f5ab5ade4aa3cf
Fostix <accretiondiskthat@gmail.com>
2021-11-29 21:45:23 +0500

    task09lastnumberpriplenumber

---
+++ task08From1EvenNumberToWhutYouWant/Program.cs
@@ -0,0 +1,36 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("enter finite number: ");
+int finite = int.Parse(Console.ReadLine());
+Console.WriteLine(1);
+
+for (int i = 1; i <= finite; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/* Console.WriteLine("even enter number: ");
+int num = int.Parse(Console.ReadLine());
+int
+
+
+for (int shownumb = 1; shownumb < 10; shownumb++)
+{
+    Console.WriteLine(shownumb, "evennumb");
+}
+ */
+
+
---
+++ task09ShowLastNumberTripleNumber/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("enter triple number: ");
+int triple = int.Parse(Console.ReadLine());
+
+int tripleW = triple % 10;
+
+Console.Write("ones number: ");
+Console.WriteLine(tripleW);
