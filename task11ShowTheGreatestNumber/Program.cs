308d603a6402e36057002c3833032e6d051fc5f2
Fostix <accretiondiskthat@gmail.com>
2021-12-01 17:36:44 +0500

    task11 show the greatest number

---
+++ task11ShowTheGreatestNumber/Program.cs
@@ -0,0 +1,65 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int a = new Random().Next(10, 100),
+b = new Random().Next(10, 100),
+c = new Random().Next(10, 100),
+d = new Random().Next(10, 100),
+f = new Random().Next(10, 100),
+e = new Random().Next(10, 100),
+g = new Random().Next(10, 100),
+l = new Random().Next(10, 100),
+k = new Random().Next(10, 100),
+j = new Random().Next(10, 100),
+h = new Random().Next(10, 100),
+y = new Random().Next(10, 100);
+
+int q = a;
+if (b > q) q = b;
+if (c > q) q = c;
+if (d > q) q = d;
+if (f > q) q = f;
+if (e > q) q = e;
+if (g > q) q = g;
+if (l > q) q = l;
+if (k > q) q = k;
+if (j > q) q = j;
+if (h > q) q = h;
+if (y > q) q = y;
+
+
+Console.WriteLine(q);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+Console.Clear();
+int xa = 10, xb = 0,
+    xs = 9, xss = 0,
+    xe = 100, xee = 0;
+
+
+Console.SetCursorPosition(xs, xss);
+Console.Write("<");
+
+Console.SetCursorPosition(xe, xee);
+Console.Write(">");
+
+while (xa < 100)
+{
+    Console.SetCursorPosition(xa, xb);
+    Console.Write("_");
+    xa++;
+}
+ */
