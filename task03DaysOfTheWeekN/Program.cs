47c92628c1dba5bf428140cf6411bbda8d678b26
Fostix <accretiondiskthat@gmail.com>
2021-12-02 20:33:56 +0500

    task16 day  weekend need fix

---
+++ task03DaysOfTheWeekN/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] arrayD = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+Console.Write("Enter number day: ");
+int Day = int.Parse(Console.ReadLine());
+
+if (Day < 32)
+{
+    Day = Day - 1;
+    while (Day > 6)
+    {
+        Day = Day - 7;
+    }
+    Console.WriteLine($"Day: {arrayD[Day]}");
+}
+else
+{
+    Console.WriteLine("incorrect day");
+}
---
+++ task16weekend/Program.cs
@@ -0,0 +1,24 @@
+﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+string[] arrayD = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+Console.Write("Enter number day: ");
+int Day = int.Parse(Console.ReadLine());
+
+if (Day < 32)
+{
+    Day = Day - 1;
+    while (Day > 6)
+    {
+        Day = Day - 7;
+    }
+    if (Day > 4)
+    {
+        Console.WriteLine($"Day weekend: {arrayD[Day]}");
+        
+    }
+    Console.WriteLine($"Day: {arrayD[Day]}");
+}
+else
+{
+    Console.WriteLine("incorrect day");
+}
