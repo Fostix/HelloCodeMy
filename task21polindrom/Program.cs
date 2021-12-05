b7f82cbb8278acd5022e129782746e6b248ce3d4
Fostix <accretiondiskthat@gmail.com>
2021-12-05 20:46:19 +0500

    task21polindrom

--- task21polindrom/Program.cs
+++ task21polindrom/Program.cs
@@ -2,37 +2,8 @@
 Console.WriteLine("enter number");
 int test = int.Parse(Console.ReadLine());
 
-
-
-
-if (test > 100000)
+if (test % 10 == test / 10000 && test % 100 / 10 == test / 1000 % 10) Console.WriteLine("P");
+else
 {
-    int testlast = test / 10000;
-    int first = test % 10;
-    Console.WriteLine(testlast);
-    Console.WriteLine(first);
-    if (testlast == first)
-    {
-        Console.WriteLine("yes");
-    }
+    Console.WriteLine("Not");
 }
-
-
-
-/* Console.WriteLine(test);
-
-int sec = trip % 100; // остаток не просто деление
-sec = sec / 10; // обычное деление
-
-Console.Write("second number from triple number: ");
-Console.WriteLine(sec); */
-
-/* int numTr = new Random().Next(100,999);
-Console.WriteLine(numTr);
-
-int numLast = numTr % 10;
-numTr = numTr / 100;
-numTr = numTr * 10;
-numTr = numLast + numTr;
-
-Console.WriteLine(numTr); */
--- task22findPoint/Program.cs
+++ task22findPoint/Program.cs
@@ -1,3 +1,21 @@
 ﻿// Найти расстояние между точками в пространстве 2D/3D
+/* 
+int X = 89, Y = 43;
+Console.WriteLine(X - Y); //2D */
 
-int A
+
+
+int Xa = 3, Xb = 4, Xc = 6,
+    Ya = 6, Yb = 2, Yc = 7;
+    
+Console.WriteLine(X + Y); //3D
+
+
+
+
+
+
+
+
+
+
