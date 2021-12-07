c9f9653f03ce2ed2b6e09d8159d80898d857dd67
Fostix <accretiondiskthat@gmail.com>
2021-12-07 20:35:10 +0500

    task 23 table square

--- task22findPoint/Program.cs
+++ task22findPoint/Program.cs
@@ -6,6 +6,15 @@ Console.WriteLine(X - Y); //2D
 
 
 
+// Показать таблицу квадратов чисел от 1 до N
 
+Console.WriteLine("enter: ");
+int hello = int.Parse(Console.ReadLine());
+hello = hello + 1;
+for (int i = 1; i < hello; i++)
+{
+    int  a = i * i;
+    Console.WriteLine(a);
+}
 
 
