bdd123ed5c470c95ab7be828f3c5f1c83f3285ba
Fostix <accretiondiskthat@gmail.com>
2021-12-07 20:39:55 +0500

    task 24 find cube number

---
+++ task24findcubenum/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+Console.Write("Enter number: ");
+int length = int.Parse(Console.ReadLine());
+length = length + 1;
+for (int i = 0; i < length; i++)
+{
+    int z = i * i * i;
+    Console.WriteLine(z);
+}
