e16d20cee4a7f66b2ee4a775d132ed2b4842e5a7
Fostix <accretiondiskthat@gmail.com>
2021-12-11 18:52:33 +0500

    task29 factorial

--- task21polindrom/Program.cs
+++ task21polindrom/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("enter number");
 int test = int.Parse(Console.ReadLine());
 
-if (test % 10 == test / 10000 && test % 100 / 10 == test / 1000 % 10) Console.WriteLine("P");
+if (test % 10 == test / 10000 && test % 100 / 10 == test / 1000 % 10) Console.WriteLine("Polindrom");
 else
 {
     Console.WriteLine("Not");
