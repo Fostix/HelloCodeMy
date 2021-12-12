9efca1518dc3758f7382574026cf7b3e1ff26853
Fostix <accretiondiskthat@gmail.com>
2021-12-12 21:43:30 +0500

    tast34 Change element array to negative

---
+++ task34ChangeElementMassiveToNegative/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу замену элементов массива на противоположные.
+
+int[] array = new int[2];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 9);
+    Console.Write(array[i] + "     ");
+    if (array[i] < 0)
+    {
+        array[i] =  -array[i];
+        Console.WriteLine(array[i]);
+    }
+    else
+    {
+        array[i] = -array[i];
+        Console.WriteLine(array[i]);
+    }
+}
