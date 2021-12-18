dfa529e44b8f40af94917f541679b723417c2439
Fostix <accretiondiskthat@gmail.com>
2021-12-18 07:47:03 +0500

    make task 443 convert decimal to binary

---
+++ task43ConvertDecimalToBinary/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу преобразования десятичного числа в двоичное.
+int[] arra = new int[1000];
+int first = 999,
+ second = 2,
+ i;
+
+for (i = 0; first > 0; i++)
+{
+    arra[i] = first % second;
+    first = first / second;
+
+}
+for (i = i - 1; i >= 0; i--)
+{
+    Console.Write(arra[i]);
+}
---
+++ task45Fibonacci/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать числа Фибоначчи.
+
+
+int Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 99; i++)
+{
+    Console.WriteLine($"({i}) = {Fibonacci(i)}");
+}
