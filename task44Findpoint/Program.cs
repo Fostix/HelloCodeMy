7af927ca32e195bf34607157f35935fc343333ad
Fostix <accretiondiskthat@gmail.com>
2021-12-18 12:48:56 +0500

    task 50 convert even numbers to negative

---
+++ task44Findpoint/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами.
+int m = 10;
+int n = 10;
+
+
+int[,] array = new int[m, n];
+
+void FillArray(int[,] fill)
+{
+    for (int i = 0; i < fill.Length; i++)
+    {
+        for (int j = 0; j < fill.Length; j++)
+        {
+            
+        }
+    }
+}
+
+
