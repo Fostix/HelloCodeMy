37ae7f50ac78f8139e6e699c7157c25b2d6e447a
Fostix <accretiondiskthat@gmail.com>
2022-01-01 17:50:02 +0500

    task 56 select first and last row

--- task56SelectFirstAndLastRow/Program.cs
+++ task56SelectFirstAndLastRow/Program.cs
@@ -1,7 +1,14 @@
 ﻿// Написать программу, которая обменивает элементы первой строки и последней строки.
 
-int[,] array = new int[5, 5];
-int[,] secArray = new int[5, 5];
+Console.Write("Enter row array: ");
+int columnAr = int.Parse(Console.ReadLine());
+Console.Write("Enter column array: ");
+int rowAr = int.Parse(Console.ReadLine());
+
+
+
+int[,] array = new int[rowAr, columnAr];
+int[] tempArray = new int[columnAr];
 
 
 void FillArray(int[,] fill)
@@ -11,27 +18,38 @@ void FillArray(int[,] fill)
         for (int column = 0; column < fill.GetLength(1); column++)
         {
             fill[row, column] = new Random().Next(0, 10);
-            Console.Write(fill[row, column] + " ");
         }
-        Console.WriteLine();
     }
 }
 
-void SelectRow(int[,] mrArray, int[,] mrBrainTemt)
+void SelectRow(int[,] mrArray, int[] mrBrainTemt, int rowArray)
 {
-    for (int row = 0; row < mrArray.GetLength(0); row++)
+    for (int column = 0; column < mrBrainTemt.Length; column++)
     {
-        for (int column = 0; column < mrArray.GetLength(1); column++)
+        mrBrainTemt[column] = mrArray[0, column];
+        mrArray[0, column] = mrArray[rowArray - 1, column];
+        mrArray[rowArray - 1, column] = mrBrainTemt[column];
+    }
+}
+
+
+
+void PrintArray(int[,] print)
+{
+    for (int row = 0; row < print.GetLength(0); row++)
+    {
+        for (int column = 0; column < print.GetLength(1); column++)
         {
-            mrBrainTemt[row, column] = mrArray[row, column];
-            Console.Write(mrBrainTemt[row, column]);
+            Console.Write(print[row, column] + " ");
         }
         Console.WriteLine();
     }
 }
-
 
 
 FillArray(array);
-SelectRow(array, secArray);
+PrintArray(array);
+SelectRow(array, tempArray, rowAr);
+Console.WriteLine();
+PrintArray(array);
 
