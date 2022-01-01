353cd824603047f51fe44ea3ef9d0724eb60e2b7
Fostix <accretiondiskthat@gmail.com>
2022-01-01 10:53:55 +0500

    need add maked tasks

---
+++ testbook/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+// double x = 23,
+// y = 5,
+// q = 0;
+
+// while (x >= y)
+// {
+//     x = x - y;
+//     q = q + 1;
+// }
+
+// Console.Write($"r = {x}, q = {q}");
+
+// int a = 1,
+// b = 1;
+
+// do
+// {
+//     a = a * 2;
+//     b = b + a;
+// } while (a != 128);
+
+// Console.WriteLine(b);
+
+
+// double i = 1;
+// double x = 10;
+
+// do
+// {
+//     i = i + 1;
+//     x = x + 0.1 * x;
+// } while (x <= 25);
+
+// Console.WriteLine(i);
+
+int n = 3;
+double a = 4,
+y = 1;
+
+for (int i = 1; i >= n; i++)
+{
+    y = y * a;
+}
