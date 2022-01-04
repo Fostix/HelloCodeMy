52964c3797e5fc4d85921bca174f468651f05e7e
Fostix <accretiondiskthat@gmail.com>
2022-01-04 08:26:13 +0500

    try push

---
+++ task66showNaturalNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать натуральные числа от 1 до N, N задано.
+
+Console.Write("Enter finish number: ");
+int finish = int.Parse(Console.ReadLine());
+
+
+int RecursionMinus(int finish)
+{
+    if (finish == 0)
+    {
+        return finish;
+    }
+    Console.Write(RecursionMinus(finish - 1) + " ");
+    return finish;
+}
+
+RecursionMinus(finish + 1);
---
+++ task67ShowNaturalNumberMinus/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от N до 1, N задано.
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+int Rec(int num)
+{
+    if (num == 0) return 0;
+    Console.Write(num + " ");
+    return Rec(num - 1);
+}
+
+Rec(number);
