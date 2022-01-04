f3dd3e643e717cae33071b5b8827c1fa3b20823c
Fostix <accretiondiskthat@gmail.com>
2022-01-04 10:08:55 +0500

    flowchart task 68

--- task68ShowNmberStartNEndM/Program.cs
+++ task68ShowNmberStartNEndM/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать натуральные числа от M до N, N и M заданы.
+
+Console.Write("Enter firstnumber: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+
+int Rec(int start, int finish)
+{
+    if (start > finish)
+    {
+        if (start == finish + 1)
+        {
+            return 0;
+        }
+        Console.Write(start + " ");
+        return Rec(finish, start + 1);
+    }
+
+    if (start == finish + 1)
+    {
+        return finish;
+    }
+    Console.Write(finish + " ");
+    return Rec(start, finish - 1);
+}
+
+Rec(firstNumber, secondNumber);
